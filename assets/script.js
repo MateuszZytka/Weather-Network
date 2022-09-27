6a87428690ec9398fb2709c866b3db82c0d5d7ad
MateuszZytka <109117198+MateuszZytka@users.noreply.github.com>
2022-09-27 19:22:34 -0400

    added 5 day forecast and media query for 576px size

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,37 @@
-let weather = {
-    "apikey": "35a55f32ca0b9555d78059eeffda6096",
+const apikey = "35a55f32ca0b9555d78059eeffda6096"
+let timeDisplayEl = $('#date');
+let timeDisplayEl1 = $('#date1');
+let timeDisplayEl2= $('#date2');
+let timeDisplayEl3 = $('#date3');
+let timeDisplayEl4 = $('#date4');
+let timeDisplayEl5 = $('#date5');
+
+
+
+
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY a');
+    let day1Date = moment().add(1, 'days').format('MM-DD-YYYY')
+    let day2Date = moment().add(2, 'days').format('MM-DD-YYYY')
+    let day3Date = moment().add(3, 'days').format('MM-DD-YYYY')
+    let day4Date = moment().add(4, 'days').format('MM-DD-YYYY')
+    let day5Date = moment().add(5, 'days').format('MM-DD-YYYY')
+    timeDisplayEl.text(rightNow);
+    timeDisplayEl1.text(day1Date);
+    timeDisplayEl2.text(day2Date);
+    timeDisplayEl3.text(day3Date);
+    timeDisplayEl4.text(day4Date);
+    timeDisplayEl5.text(day5Date);
+
+  }
+
+const weather = {
     fetchWeather: function(city){
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
              + city
               + "&units=metric&appid=" 
-              + this.apikey
+              + apikey
         )
         .then((response) => {
             if (!response.ok) {
@@ -14,7 +40,7 @@ let weather = {
             }
             return response.json();
           })
-          .then((data) => this.displayWeather(data));
+          .then((data) => this.displayWeather(data))
     },
     displayWeather: function(data){
         const { name } = data;
@@ -23,7 +49,7 @@ let weather = {
         const { speed } = data.wind
         console.log(name, icon, description, temp, humidity, speed)
         document.querySelector("#city").innerText = name
-        document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector("#temp").innerText = "Temp :" + temp + "°C"
         document.querySelector("#humidity").innerText = "Humidity: " + humidity + " %"
         document.querySelector("#speed").innerText = "Wind Speed: " + speed + " Km/H"
@@ -31,11 +57,58 @@ let weather = {
     },
     search: function () {
         this.fetchWeather(document.querySelector("#searchBar").value)
-    }
+    },
+    forecast: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/forecast?q="
+             + city
+            + "&units=metric&appid="
+            + apikey
+        )
+        .then((response) => {
+            if (!response.ok) {
+              alert("No weather found.");
+              throw new Error("No weather found.");
+            }
+            return response.json();
+          })
+
+        .then(function(data){
+            console.log(data)
+           
+            for (let i = 2, j = 1; i < 41, j < 6; i = i + 8, j++){
+                
+                const { icon } = data.list[i].weather[0]
+                const { temp, humidity} = data.list[i].main
+                const { speed } = data.list[i].wind
+                console.log(icon, temp, humidity, speed)
+                console.log(j)
+                document.querySelector("#icon" + [j]).src = "https://openweathermap.org/img/wn/" + icon + ".png";
+                document.querySelector("#temp" + [j]).innerText = "Temp :" + temp + "°C"
+                document.querySelector("#humidity" + [j]).innerText = "Humidity: " + humidity + " %"
+                document.querySelector("#speed" + [j]).innerText = "Wind Speed: " + speed + " Km/H"
+        }
+    });
+}
 }
 
+
+
 document.querySelector("#search").addEventListener("click", function(){
+
+    localStorage.setItem("City", (document.querySelector("#searchBar").value))
+    let listItem = $("<li>");
+    listItem.on("click", function(){
+        weather.search()
+        weather.forecast()
+    })
+    document.querySelector("#list").appendChild.listItem
     weather.search()
+    weather.forecast((document.querySelector("#searchBar").value))
+
 })
 
+
+displayTime()
 weather.fetchWeather("Atlanta")
+weather.forecast("Atlanta")
